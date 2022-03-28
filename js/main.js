3b658ebaf69b67a9af2dd70dd9ccaccc4709d230
yarovik1982 <yar.oleg2018@yandex.ru>
2022-03-28 20:23:16 +0300

    верстка и функционал бургерМеню

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("click", function(event){
+   const el = event.target;
+   if(el.closest("[data-burger]")){
+      showMobileMenu();
+      document.querySelectorAll("[data-burger]").forEach(item=>{
+         item.classList.toggle("toggle");
+      })
+   }
+}, true)
+
+const mobileMenu = document.querySelector(".nav__mobile-menu");
+
+function showMobileMenu () {
+   mobileMenu.classList.toggle('active');
+}
